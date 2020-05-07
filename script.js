4906843ae5df2ac2332f4f71b0af3d22b64288b6
Claire Cheng <clairecheng@Claires-MBP-2.lan>
2020-05-07 12:24:35 -0400

    added files

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 document.getElementById('img1').addEventListener('click',showImg);
+document.getElementById('img2').addEventListener('click',showImg2);
+document.getElementById('exitbtn').addEventListener('click',error);
+function error(){
+  alert("Warning! Please return to your fishtank!")
+}
+
 
 function showImg(){
   var img=document.getElementById('img1');
   img.src="002_hvr.jpg";
+  document.getElementById('caption1').innerHTML="Content Farm";
+
+}
+
+function showImg2(){
+  var img_2=document.getElementById('img2');
+  img_2.src="001_hvr.jpg";
+  document.getElementById('caption2').innerHTML="Distruction";
 }
